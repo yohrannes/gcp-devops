090c9690777e9a439d688fee213ade97c5abaa1f
yohrannes <yohrannes@gmail.com>
2024-08-16 19:02:22 -0300

    update

--- startup-script.sh
+++ startup-script.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # Instalando docker engine e executando a aplicação em container.
-sudo apt-get update
-sudo apt-get install -y ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo tee /etc/apt/keyrings/docker.asc
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo systemctl enable docker
-sudo systemctl start docker
-sudo docker run -d -p 5000:5000 yohrannes/coodesh-challenge
+apt-get update
+apt-get install -y ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | tee /etc/apt/keyrings/docker.asc
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+systemctl enable docker
+systemctl start docker
+docker run -d -p 5000:5000 yohrannes/coodesh-challenge
 
 # Instalando nginx como proxy da porta 80 para a 5000 da aplicação.
-sudo apt-get install -y nginx
-sudo tee /etc/nginx/sites-available/default <<EOF
+apt-get install -y nginx
+tee /etc/nginx/sites-available/default <<EOF
 server {
     listen 80;
     server_name _;
@@ -28,5 +28,5 @@ server {
     }
 }
 EOF
-sudo systemctl restart nginx
-sudo systemctl enable nginx
+systemctl restart nginx
+systemctl enable nginx
